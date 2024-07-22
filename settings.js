f20bb0475b38711ea927b12526b74b943da8c6f5
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-07-22 19:44:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '|°ＢＬＡＣＫ | ＡＬＰＨＡ';
 global.owner = process.env.OWNER_NUMBER  || '94727270908' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=CBlDmB5R#EVxnqYvXGHBkejMUy7Oa2RXKwZEdyd_g1k9E7pJsIQM' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=7VMQXDAQ#pS4USNapmZ_W-PgKXQKTJZva-xSaI_gPYYl4lOTHZ0w' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
